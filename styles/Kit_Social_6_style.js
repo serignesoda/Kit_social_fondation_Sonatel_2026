44fb21aa5dcb48619540ecf01e8180a563e2e555
serignesoda <serignesoda@gmail.com>
2026-09-06 15:40:15 +0000

    Add files via upload

---
+++ styles/Kit_Social_6_style.js
@@ -0,0 +1,40 @@
+var size = 0;
+var placement = 'point';
+
+var style_Kit_Social_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Tahoma\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if (feature.get("KIT") !== null) {
+        labelText = String(feature.get("KIT"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 13.2 + size, points: 5,
+            radius2: 6.6, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,127,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 14.4 + size, points: 5,
+            radius2: 7.2, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(255,127,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
